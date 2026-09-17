628f19fb978008d1bcd556cd7e622759011f98bd
Santhosh Kumar M <santhosh.skm.28@gmail.com>
2026-09-17 12:28:06 +0530

    Add: reorder-linked-list - submission-0

---
+++ Data Structures & Algorithms/reorder-linked-list/submission-0.js
@@ -0,0 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     constructor(val = 0, next = null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {ListNode} head
+     * @return {void}
+     */
+    reorderList(head) {
+        if (!head) return;
+
+        const nodes = [];
+        let cur = head;
+        while (cur) {
+            nodes.push(cur);
+            cur = cur.next;
+        }
+
+        let i = 0,
+            j = nodes.length - 1;
+        while (i < j) {
+            nodes[i].next = nodes[j];
+            i++;
+            if (i >= j) break;
+            nodes[j].next = nodes[i];
+            j--;
+        }
+
+        nodes[i].next = null;
+    }
+}
