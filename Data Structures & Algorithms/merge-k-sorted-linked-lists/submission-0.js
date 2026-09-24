e5c8686a08786172c552003c23cfd59415e7d5db
Santhosh Kumar M <santhosh.skm.28@gmail.com>
2026-09-24 14:21:45 +0530

    Add: merge-k-sorted-linked-lists - submission-0

---
+++ Data Structures & Algorithms/merge-k-sorted-linked-lists/submission-0.js
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     constructor(val = 0, next = null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {ListNode[]} lists
+     * @return {ListNode}
+     */
+    mergeKLists(lists) {
+        let nodes = [];
+        for (let lst of lists) {
+            while (lst) {
+                nodes.push(lst.val);
+                lst = lst.next;
+            }
+        }
+        nodes.sort((a, b) => a - b);
+
+        let res = new ListNode(0);
+        let cur = res;
+        for (let node of nodes) {
+            cur.next = new ListNode(node);
+            cur = cur.next;
+        }
+        return res.next;
+    }
+}
